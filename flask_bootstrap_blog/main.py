22a9e25ec026815f11cfd32167dbe8f8b26c4e53
DominikZurawski <38438473+DominikZurawski@users.noreply.github.com>
2024-06-19 13:27:12 +0200

    Update main.py

--- flask_bootstrap_blog/main.py
+++ flask_bootstrap_blog/main.py
@@ -3,6 +3,9 @@ from post import Post
 import smtplib
 import os
 
+OWN_EMAIL = os.environ["MY_EMAIL"]
+OWN_PASSWORD = os.environ["MY_PASSWORD"]
+
 app = Flask(__name__)
 posts = Post()
 
@@ -30,20 +33,19 @@ def contact():
         return render_template("contact.html")
     elif request.method == 'POST':
         data = request.form
-        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-            connection.starttls()
-            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-            connection.sendmail(
-                from_addr=MY_EMAIL,
-                to_addrs=RECEIVER_EMAIL,
-                msg=f"Subject:Motivational Quote\n\n{random_quote["quote"]}\n \n{random_quote["author"]}")
-        print(data["name"])
-        print(data["email"])
-        print(data["phone"])
-        print(data["message"])
+        send_email(data["name"], data["email"], data["phone"], data["message"])
         return render_template("contact.html", message="Successfully sent your message")
 
-
+def send_email(name, email, phone, message):
+    # Gmail: smtp.gmail.com | port 587
+    # Hotmail: smtp.live.com
+    # Outlook: outlook.office365.com
+    # Yahoo: smtp.mail.yahoo.com
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}".encode('utf-8')
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 if __name__ == "__main__":
     app.run(debug=True)
